de8ee925e235b314e1980316f1eb261966e34ad2
unknown <computerengineering.edu.np@gmail.com>
2023-10-29 13:25:25 +0545

    intial commit to main

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("Arithmetic operators in JS");
+let a = 14;
+let b = 8;
+ console.log("a+b=", a + b);
+ console.log("a-b=", a - b);
+console.log("a/b=", a / b);
+ console.log("a%b=", a % b);
+ console.log("a*b=", a * b);
+ console.log("a>b=", a > b);
+ console.log("a<b=", a <b);
+ console.log("a>=b=", a >= b);
+ console.log("a+=b", a += b);
+console.log("a**b",a**b);
+console.log("a++=", a++);  
+console.log("a+=b", a +=b);
+console.log("--a=",--a);
+console.log("++a=",++a);
+console.log("--b=",--b);
+console.log("a--=",a--);
+console.log("--a=",--a);
+
+
+
+
+
